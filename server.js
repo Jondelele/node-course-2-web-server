26b6ac583c027e370791aa48a01eb9556b92607a
Joona Prehti <joona.prehti@student.tut.fi>
2018-07-10 21:57:46 +0300

    Testing updating heroku

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'About Page',
+    portfolioMessage: 'Portfolio page here'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
